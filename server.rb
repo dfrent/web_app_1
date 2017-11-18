e446230ca1d34e3adbbd71a3b18447013bee6e1b
cordell <cordell.perry@gmail.com>
2017-11-18 16:49:21 -0500

    redirects and links added to server/ links file in views

--- server.rb
+++ server.rb
@@ -7,3 +7,22 @@ end
 get '/portfolio' do #navigates to portfolio/gallery
  erb :gallery
 end
+
+get '/about_me' do
+  @skills = ['git', 'HTML', 'CSS', 'Ruby']
+  @interests = ['cats', 'art', 'music', 'films', 'coffee']
+  erb :about_me
+end
+
+get '/favorites' do
+  @fav_links = {'Netflix' => 'https://www.netflix.com/ca/', 'Google' => 'https://www.google.ca/?gfe_rd=cr&dcr=0&ei=AqUQWoLfOqfU8gfm24pA', 'Space Jam' => 'https://www.warnerbros.com/archive/spacejam/movie/jam.htm', 'IMDB' => 'http://www.imdb.com/?ref_=nv_home', 'Ruby Docs' => 'http://ruby-doc.org/'}
+  erb :favorites
+end
+
+get '/' do
+  redirect to('/home')
+end
+
+get '/gallery' do
+  redirect to('/portfolio')
+end
